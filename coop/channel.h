54528568d9e19f4954bebe4c645bdfa4d3dbe308
jrs <jsherwood@singlestore.com>
2025-02-28 03:13:03 +0000

    RecvChannel/SendChannel

--- coop/channel.h
+++ coop/channel.h
@@ -2,75 +2,214 @@
 
 #include "coordinator.h"
 
-// This was intended as the second-level building block for coordination and to stress the system
-// a bit. It's intended to be a semaphore, a basic
+// This is a pretty close facscimile of golang's channel implementation and is meant to be a second
+// order coordination primitive on top of the Coordinator. However while the coordinator currently
+// has some nice building block pieces including a golang select over channel implementation (e.g.,
+// (CoordinateWith), this doesn't have a `select`.
 //
-// - unidirectional channel would be fun. I think virtual inheritance could achieve this.
+// This may well be a technical dead end and anything higher level than coordinators to this degree
+// misses the point of coordinators and their intended built-in composibility; e.g. if the
+// coordinatewith mechanic isn't sufficient then what's the point? Or the channel system would
+// simply use that itself under the hood and that's okay.
+//
+// Either way it was fun making the diamond pattern work.
 //
 
 namespace coop
 {
 
-// BaseChannel is currently just a way to add a common ancestor to the template specialization.
-//
-// There's a pretty crazy amount of refactoring in general here around the coordination behaviors
-// that are 90% the same repeated 8x or so in total, potentially, that it would be nice to see.
+// NOTE: there is almost certainly a good amount of refactoring (I would hope?) around all of the
+// subtly different logic in the send/recv implementations. Or maybe not and that's why it's
+// subtly different.
 //
 struct BaseChannel
 {
-};
-
-// Channel is basically wholesale copied from golang in terms of the semantics, interface, and
-// patterns it allows.
-//
-// With respect to implementation, a channel mantains a circular queue and a pair of coordinators
-// that gate access to reads and writes respectively. The coordinators allow forming waiter queues
-// for these operations. The logic is:
-//
-// (1)  Check if the operation would block; if not, skip to step 6
-// (2)  Acquire the coordinator for the operation
-// (3)  Perform the operation
-// (4)  Release the coordinator
-// (5)  Skip to step 8
-// (6)  Perform the operation
-// (7)  If the opposing coordinator is held, release it
-//
-// There is also a shutdown state that prevents using the channel further for sending, including
-// releasing any receivers with failure.
-//
-template<typename T>
-struct Channel : BaseChannel
-{
-    Channel(Channel const& other) = delete;
-    Channel(Channel&&) = delete;
-
-    Channel(T* buffer, size_t capacity)
-    : m_buffer(buffer)
-    , m_head(0)
-    , m_tail(0)
-    , m_capacity(capacity)
-    , m_shutdown(false)
+    BaseChannel()
+    : m_shutdown(false)
     {
     }
 
-    virtual ~Channel()
+    virtual ~BaseChannel()
     {
     }
 
-    // Shutdown the channel
-    //
-    void Shutdown(Context* ctx)
+    bool IsShutdown() const
     {
-        m_shutdown = true;
-        if (m_send.IsHeld())
+        return m_shutdown;
+    }
+
+    bool Shutdown(Context* ctx)
+    {
+        if (m_shutdown)
         {
-            m_send.Release(ctx);
+            return false;
         }
+
+        m_shutdown = true;
 
         if (m_recv.IsHeld())
         {
             m_recv.Release(ctx);
         }
+        if (m_send.IsHeld())
+        {
+            m_send.Release(ctx);
+        }
+
+        return true;
+    }
+
+    Coordinator m_recv;
+    Coordinator m_send;
+
+  private:
+    bool m_shutdown;
+};
+
+template<typename T>
+struct TypedBaseChannel : public BaseChannel
+{
+  public:
+    TypedBaseChannel(TypedBaseChannel const& other) = delete;
+    TypedBaseChannel(TypedBaseChannel&&) = delete;
+
+    TypedBaseChannel(T* buffer, size_t capacity)
+    : m_buffer(buffer)
+    , m_head(0)
+    , m_tail(0)
+    , m_capacity(capacity)
+    {
+    }
+
+    virtual ~TypedBaseChannel()
+    {
+    }
+  
+    bool IsEmpty() const
+    {
+        return m_head == m_tail;
+    }
+
+    bool IsFull() const
+    {
+        auto next = (m_tail + 1) % m_capacity;
+        return next ==  m_head;
+    }
+
+  private:
+    T* m_buffer;
+    size_t m_head;
+    size_t m_tail;
+    size_t m_capacity;
+};
+
+template<typename T>
+struct RecvChannel : virtual TypedBaseChannel<T>
+{
+    using Base = TypedBaseChannel<T>;
+
+    bool TryRecv(Context* ctx, T& value /* out */)
+    {
+        if (Base::IsEmpty())
+        {
+            return false;
+        }
+
+        RecvImpl(ctx, value);
+
+        if (Base::IsEmpty())
+        {
+            Base::m_recv.Acquire(ctx);
+        }
+
+        if (Base::m_send.IsHeld())
+        {
+            Base::m_send.Release(ctx);
+        }
+
+        return true;
+    }
+    
+    // Mirror image of the above but we handle shutdown differently
+    //
+    bool Recv(Context* ctx, T& value /* out */)
+    {
+        if (TryRecv(ctx, value))
+        {
+            return true;
+        }
+
+        Base::m_recv.Acquire(ctx);
+
+        // This is possible in the shutdown case
+        //
+        if (!RecvImpl(value))
+        {
+            assert(Base::IsShutdown());
+            Base::m_recv.Release(ctx);
+            return false;
+        }
+
+        // If we are shutdown, no more things can come through so signal anyone else who is waiting
+        // on the coordinator; alternatively, if there is more to recv, then let go of it for the
+        // next coordinatee.
+        //
+        if ((Base::IsShutdown() && Base::IsEmpty()) || !Base::IsEmpty())
+        {
+            Base::m_recv.Release(ctx);
+        }
+
+        if (Base::m_send.IsHeld() && !Base::IsFull())
+        {
+            Base::m_send.Release(ctx);
+        }
+
+        return true;
+    }
+
+  protected:
+    bool RecvImpl(T& value)
+    {
+        if (Base::IsEmpty())
+        {
+            return false;
+        }
+
+        value = Base::m_buffer[Base::m_head++];
+        if (Base::m_head == Base::m_capacity)
+        {
+            Base::m_head = 0;
+        }
+        return true;
+    }
+};
+
+template<typename T>
+struct SendChannel : virtual TypedBaseChannel<T>
+{
+    using Base = TypedBaseChannel<T>;
+
+    bool TrySend(Context* ctx, T value)
+    {
+        if (Base::IsShutdown() || Base::IsFull())
+        {
+            return false;
+        }
+
+        SendImpl(value);
+        
+        if (Base::IsFull())
+        {
+            Base::m_send.Acquire(ctx);
+        }
+
+        // Note that this is not the same as the non-try case
+        //
+        if (Base::m_recv.IsHeld())
+        {
+            Base::m_recv.Release(ctx);
+        }
+        return true;
     }
 
     // Push adds the given item to the channel, returning failure if the channel was shutdown
@@ -85,172 +224,54 @@ struct Channel : BaseChannel
 
         // Blocking codepath: note that IsShutdown() can get flipped while we were coordinating
         //
-        m_send.Acquire(ctx);
-        if (IsShutdown())
+        Base::m_send.Acquire(ctx);
+        if (Base::IsShutdown())
         {
-            m_send.Release(ctx);
+            Base::m_send.Release(ctx);
             return false;
         }
 
-        SendImpl(value);
+        Base::SendImpl(value);
         
         // If there is space, let go of the coordinator so that the preconditions still stand. This
         // in theory wouldn't happen today but if we change the unblock mechanics to not just
         // chain immediately it's very possible for multiple Recvs before this wakes up holding the
         // coordinator.
         //
-        if (!IsFull())
+        if (!Base::IsFull())
         {
-            m_send.Release(ctx);
+            Base::m_send.Release(ctx);
         }
 
-        if (m_recv.IsHeld() && !IsEmpty())
+        if (Base::m_recv.IsHeld() && !Base::IsEmpty())
         {
-            m_recv.Release(ctx);
+            Base::m_recv.Release(ctx);
         }
-    }
-
-    // Mirror image of the above but we handle shutdown differently
-    //
-    bool Recv(Context* ctx, T& value /* out */)
-    {
-        if (TryRecv(ctx, value))
-        {
-            return true;
-        }
-
-        m_recv.Acquire(ctx);
-
-        // This is possible in the shutdown case
-        //
-        if (!RecvImpl(value))
-        {
-            assert(IsShutdown());
-            m_recv.Release(ctx);
-            return false;
-        }
-
-        // If we are shutdown, no more things can come through so signal anyone else who is waiting
-        // on the coordinator; alternatively, if there is more to recv, then let go of it for the
-        // next coordinatee.
-        //
-        if ((Shutdown() && IsEmpty()) || !IsEmpty())
-        {
-            m_recv.Release(ctx);
-        }
-
-        if (m_send.IsHeld() && !IsFull())
-        {
-            m_send.Release(ctx);
-        }
-
-        return true;
-    }
-
-    bool TrySend(Context* ctx, T value)
-    {
-        if (IsShutdown() || IsFull())
-        {
-            return false;
-        }
-
-        SendImpl(value);
-        
-        if (IsFull())
-        {
-            m_send.Acquire(ctx);
-        }
-
-        // Note that this is not the same as the non-try case
-        //
-        if (m_recv.IsHeld())
-        {
-            m_recv.Release(ctx);
-        }
-        return true;
-    }
-
-    bool TryRecv(Context* ctx, T& value /* out */)
-    {
-        if (IsEmpty())
-        {
-            return false;
-        }
-
-        RecvImpl(ctx, value);
-
-        if (IsEmpty())
-        {
-            m_recv.Acquire(ctx);
-        }
-
-        if (m_send.IsHeld())
-        {
-            m_send.Release(ctx);
-        }
-
-        return true;
-    }
-
-    bool IsShutdown() const
-    {
-        return m_shutdown;
     }
 
   protected:
-    // _Impl methods are both step 1 (check if it is possible to do the operation nonblockingly)
-    // and step 4 (just do the operation)
-    //
-
     bool SendImpl(Context* ctx, T value)
     {
-        if (IsFull())
+        if (Base::IsFull())
         {
             return false;
         }
-        m_buffer[m_tail++] = value;
-        if (m_tail == m_capacity)
+        Base::m_buffer[Base::m_tail++] = value;
+        if (Base::m_tail == Base::m_capacity)
         {
-            m_tail = 0;
+            Base::m_tail = 0;
         }
         return true;
     }
+};
 
-    bool RecvImpl(T& value)
+template<typename T>
+struct Channel : RecvChannel<T>, SendChannel<T>
+{
+    Channel(T* buffer, size_t capacity)
+    : TypedBaseChannel<T>(buffer, capacity)
     {
-        if (IsEmpty())
-        {
-            return false;
-        }
-
-        value = m_buffer[m_head++];
-        if (m_head == m_capacity)
-        {
-            m_head = 0;
-        }
-        return true;
     }
-
-    bool IsFull() const
-    {
-        auto next = (m_tail + 1) % m_capacity;
-        return next ==  m_head;
-    }
-
-    bool IsEmpty() const
-    {
-        return m_head == m_tail;
-    }
-
-  private:
-    Coordinator m_send;
-    Coordinator m_recv;
-
-    T* m_buffer;
-    size_t m_head;
-    size_t m_tail;
-    size_t m_capacity;
-    bool m_shutdown;
 };
 
 template<>
@@ -259,7 +280,6 @@ struct Channel<void> : BaseChannel
     Channel(int capacity, int used = 0)
     : m_size(used)
     , m_capacity(capacity)
-    , m_shutdown(false)
     {
         assert(m_capacity < m_size);
     }
@@ -363,29 +383,6 @@ struct Channel<void> : BaseChannel
         return true;
     }
 
-    void Shutdown(Context* ctx)
-    {
-        if (IsShutdown())
-        {
-            return;
-        }
-        m_shutdown = true;
-        if (m_send.IsHeld())
-        {
-            m_send.Release(ctx);
-        }
-
-        if (m_recv.IsHeld())
-        {
-            m_recv.Release(ctx);
-        }
-    }
-
-    bool IsShutdown() const
-    {
-        return m_shutdown;
-    }
-
     bool IsEmpty() const
     {
         return m_size == 0;
@@ -420,8 +417,6 @@ private:
     int m_size;
     int m_capacity;
     bool m_shutdown;
-    Coordinator m_send;
-    Coordinator m_recv;
 };
 
 // SizedChannel is a channel and requires no virtualization on the hot path for channel use; i's
